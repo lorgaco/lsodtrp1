119b82e98c8c458dc912228868fc62bfc8a43d62
lorgaco <lorenzo.gaco@gmail.com>
2014-02-26 11:51:43 +0100

    Client started

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,60 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Client {
+	public static void main(String args [] ) {
+		if(args.length<1) System.err.println("Not enough arguments");
+		else{
+			System.out.println("createCommServer");
+		}
+		BufferedReader brComand = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String strComand[] = brComand.readLine().split(" ");
+			if(strComand.length<1){
+				System.err.println("Not enough arguments");
+			}
+			else{
+				String method = args[0].toString().toUpperCase();
+				if(method.equals("NUEVO")){
+					if(args.length<3) System.err.println("Not enough arguments");
+					else{
+						String designation = args[1].toString();
+						int maximum = Integer.parseInt(args[2]);
+						int result = nuevo(designation,maximum);
+						System.out.println(result);
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
+	}
+	
+	private static int nuevo(String designation, int maximum){
+		System.out.println(designation + " " + maximum);
+		ByteArrayOutputStream baParams = new ByteArrayOutputStream();
+		DataOutputStream dtParams = new DataOutputStream(baParams);
+		try {
+			dtParams.writeUTF(designation);
+			dtParams.writeInt(maximum);
+		} catch (IOException e) {
+			System.err.println("ERROR: " + e.getMessage());
+			return Data.INTERNAL_ERROR;
+		}
+
+		Message msg = new Message();
+		byte[] byParams = baParams.toByteArray();
+		msg.setbyArguments(byParams);
+		msg.setiLengthArgs(byParams.length);
+		msg.setiIdMethod(Data.NUEVO);
+		
+		
+		
+		return Data.OK;
+	}
 
 }
--- src/Data.java
+++ src/Data.java
@@ -1,15 +1,19 @@
 
 public class Data {
 	// socket and message parameters
-	public static final int PORT = 4000;
+	public static final int PORT = 4005;
 	public static final int MAX_ARGUMENTS_SIZE = 8000;
 	public static final int MAX_MESSAGE_SIZE = MAX_ARGUMENTS_SIZE + 16; // 4 ints
 	
 	// Local response codes
 	public static final int OK =0;
 	public static final int UNKNOWN_HOST = 1;
+	public static final int INTERNAL_ERROR = 2;
 	
 	// Message types
 	public static final int REQUEST = 0;
 	public static final int RESPONSE = 1;
+	
+	// Methods
+	public static final int NUEVO = 1;
 }
