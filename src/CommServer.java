8629451ac1d97ee60b4573d26ddd7d5a1bbe3f58
xulegaspi <xulegaspi@gmail.com>
2014-02-27 12:42:34 +0100

    Server first version

--- src/CommServer.java
+++ src/CommServer.java
@@ -6,16 +6,65 @@ public class CommServer {
 	private DatagramSocket dtSocket;
 	private int iIdMessage;
 
-	public CommServer(String ServerHostName) throws UnknownHostException, SocketException {
-		// host checking
-		ipHost = InetAddress.getByName(ServerHostName);
+	public CommServer() throws SocketException {
 		// socket creation
 		dtSocket = new DatagramSocket();
 		iIdMessage = 0;
+		
 	}
 	
-	public int getRequest(Message msRequest) {
+	public int getRequest(Message msRequest) throws IOException {
+
+		byte [] InBuffer = new byte[Data.MAX_MESSAGE_SIZE] ;
+		// create packet
+		DatagramPacket pkRequest = new DatagramPacket(InBuffer,	InBuffer.length);
+		dtSocket.receive(pkRequest);
 		
+		ByteArrayInputStream baIn = new ByteArrayInputStream(InBuffer);
+		DataInputStream dtIn = new DataInputStream(baIn);
+		
+		// extract fields
+		msRequest.setiTypeMessage(dtIn.readInt());
+		msRequest.setiIdMethod(dtIn.readInt());
+		msRequest.setiIdMessage(dtIn.readInt());
+		msRequest.setiLengthArgs(dtIn.readInt());
+		byte[] byArguments = new byte[Data.MAX_ARGUMENTS_SIZE];
+		dtIn.read(byArguments, 0, msRequest.getiLengthArgs());
+		msRequest.setbyArguments(byArguments);
+		
+		// Update iIdMessage
+		iIdMessage = msRequest.getiIdMessage();
+
 		return Data.OK;
+	}
+	
+	public int sendReply(Message msResponse) {
+		
+		msResponse.setiTypeMessage(Data.RESPONSE);
+		msResponse.setiIdMessage(iIdMessage);
+		
+		try {
+			ByteArrayOutputStream baOut = new ByteArrayOutputStream();
+			DataOutputStream dtOut = new DataOutputStream(baOut);
+			
+			// fill fields
+			dtOut.writeInt(msResponse.getiTypeMessage());
+			dtOut.writeInt(msResponse.getiIdMethod());
+			dtOut.writeInt(msResponse.getiIdMessage());
+			dtOut.writeInt(msResponse.getiLengthArgs());
+			dtOut.write(msResponse.getbyArguments(),0,msResponse.getiLengthArgs());
+			dtOut.close();
+			
+			// create packet
+			DatagramPacket pkRequest = new DatagramPacket(baOut.toByteArray(),
+															dtOut.size(), ipHost, Data.PORT);
+			// send the packet
+			dtSocket.send(pkRequest);
+			
+		} catch (IOException e) {
+			System.err.println("I/O: " + e.getMessage());
+		}
+		return Data.OK;
+		
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.SocketException;
+
 
 public class Server {
+	
+	private static CommServer csModule;
+	
+	public static void main(String args [] ) {
+		if(args.length>1) System.err.println("Not enough arguments");
+		else{
+			try {
+				csModule = new CommServer();
+			} catch (SocketException e) {
+				System.err.println("NET ERROR: " + e.getMessage());
+			}
+		}
+		//BufferedReader brComand = new BufferedReader(new InputStreamReader(System.in));
+		
+		Message msRequest = new Message();
+		try {
+			csModule.getRequest(msRequest);
+			
+			Message msResponse = new Message();
+			int requestIdMethod = msRequest.getiIdMethod();
+			byte [] bArguments = new byte[128];
+			msResponse.setiIdMethod(requestIdMethod);
+			msResponse.setbyArguments(bArguments);
+			msResponse.setiLengthArgs(bArguments.length);
+			
+			csModule.sendReply(msResponse);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
